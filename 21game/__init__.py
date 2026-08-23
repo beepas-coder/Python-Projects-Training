ae3f87f9171c47bdeb0e450367331bf84094b852
beepas-coder <ianbeepas@gmail.com>
2026-08-23 19:29:41 +0300

    Enhance player_function with input checks and readability
    Refactor player_function to improve input validation and readability.
    
    Just checks if guess contains only numbers

--- 21game/__init__.py
+++ 21game/__init__.py
@@ -1,18 +1,23 @@
 def player_function():
-    guess = input("Enter your play(1-3 consecutive nums): ")
-# are nums consecutive and from 1 to 3
-    guess_list = [int(num) for num in guess]
-    if len(guess) <= 3:
+    guess = input("Enter your play (1-3 consecutive nums): ")  # are nums consecutive and from 1 to 3
+    
+    if guess.isalnum():  # checks if guess have only numbers
+        if len(guess) > 3:
+            print("Try Again")
+            player_function()  # better readability
+            return  # for short circuit
+
+        guess_list = [int(num) for num in guess]
         for i in range(len(guess) - 1):
             if guess_list[i] == guess_list[i + 1] - 1:
-                print(guess_list[i], guess_list[i + 1] - 1)
+                print(guess_list[i], guess_list[i + 1])
                 print(True)
             else:
-                print(guess_list[i], guess_list[i + 1] - 1)
+                print(guess_list[i], guess_list[i + 1])
                 print(False)
-    else:
-        print("Try Again")
-        player_function() 
+                    
 
 def computer_function():
     pass
+
+player_function()
